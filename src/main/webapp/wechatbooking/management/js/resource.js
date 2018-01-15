e30917c5987b1cb2bf066106e1bfecfbbaad2496
lzq <605868359@qq.com>
2018-01-15 15:35:41 +0800

    lzq
    传错了

--- src/main/webapp/wechatbooking/management/js/resource.js
+++
@@ -1,6 +0,0 @@
-//公告
-var RESOURCE_PUBLIC_ANNOUNCEMENT = "时光静好，与君语；细水流年，与君同。—— TCSL";
-//nginx访问域名
-var RESOURCE_NGINX_DOMAIN_NAME = "http://192.168.9.136:808";
-//工程名
-var RESOURCE_PROJECT_NAME = "";
